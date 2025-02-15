ebd5649c08baf855d6f399e581494daa25b19387
Paulo Bacelar <paulobacelara@gmail.com>
2025-02-15 09:33:51 -0300

    feat: rotates between user agents and implements undetected chromedriver

--- src/scraping/main.py
+++ src/scraping/main.py
@@ -1,33 +1,32 @@
 from random import randint, choice
 from time import sleep
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+import undetected_chromedriver as uc
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
 
-USER_AGENTS = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-]
 
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+def get_user_agent():
+    software_names = [SoftwareName.CHROME.value]
+    operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
+    user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+    return user_agent_rotator.get_random_user_agent()
 
-user_agent = choice(USER_AGENTS)
-chrome_options.add_argument(f"user-agent={user_agent}")
-
+def get_chrome_options():
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument(f"user-agent=choice{get_user_agent()}")
+    return chrome_options
 
 def launch_browser(zone, district):
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-
+    driver = uc.Chrome(options=get_chrome_options())
     website = f"https://www.zapimoveis.com.br/venda/imoveis/sp+sao-paulo+{zone}+{district}"
     driver.get(website)
     sleep(sleep_time)
     driver.quit()
+
 
 # Mock variables, get from ./districts.py later
 districts = {
